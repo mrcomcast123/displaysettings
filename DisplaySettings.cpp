017c3e7585e4649305ef6b3f77a0feaa1ac9cc13
mrollins <mark_rollins@cable.comcast.com>
2019-03-20 18:07:34 -0400

    iarm all hooked up resolutionChanged event now working whitespace tabs to spaces

--- DisplaySettings.cpp
+++ DisplaySettings.cpp
@@ -1,122 +1,178 @@
 #include "DisplaySettings.h"
 
+#define MYLOG(...) fprintf(logger, __VA_ARGS__); fflush(logger);        
+
+#ifdef DS_FOUND
+#include "dsMgr.h"
+#include "libIBusDaemon.h"
+#include "host.hpp"
+#include "exception.hpp"
+#include "videoOutputPort.hpp"
+#include "videoOutputPortType.hpp"
+#include "videoOutputPortConfig.hpp"
+#include "videoResolution.hpp"
+#include "audioOutputPort.hpp"
+#include "audioOutputPortType.hpp"
+#include "audioOutputPortConfig.hpp"
+#include "manager.hpp"
+#include "dsUtl.h"
+#include "dsError.h"
+#include "list.hpp"
+#include "libIBus.h"
+#include "dsDisplay.h"
+
+#define IARM_CHECK(FUNC) \
+  if ((res = FUNC) != IARM_RESULT_SUCCESS) { \
+    MYLOG("DisplaySettings %s: %s\n", #FUNC, \
+        res == IARM_RESULT_INVALID_PARAM ? "invalid param" : ( \
+        res == IARM_RESULT_INVALID_STATE ? "invalid state" : ( \
+        res == IARM_RESULT_IPCCORE_FAIL ? "ipcore fail" : ( \
+        res == IARM_RESULT_OOM ? "oom" : "unknown")))); \
+  } else { \
+    MYLOG("DisplaySettings %s: success\n", #FUNC); \
+  }
+
+#endif
+
 namespace WPEFramework {
 
-	namespace Plugin {
+    namespace Plugin {
 
+        static FILE* logger = nullptr;
+        
         SERVICE_REGISTRATION(DisplaySettings, 1, 0);
 
-		DisplaySettings::DisplaySettings()
-			: PluginHost::JSONRPC()
-		{
-            printf("DisplaySettings CTOR!\n");
-			Register<Params, JStringArray >(_T("getConnectedVideoDisplays"), &DisplaySettings::getConnectedVideoDisplays, this);
-			Register<Params, JString>(_T("getConnectedAudioPorts"), &DisplaySettings::getConnectedAudioPorts, this);
-			Register<Params, JString>(_T("getSupportedResolutions"), &DisplaySettings::getSupportedResolutions, this);
-			Register<Params, JString>(_T("getSupportedTvResolutions"), &DisplaySettings::getSupportedTvResolutions, this);
-			Register<Params, JString>(_T("getSupportedAudioPorts"), &DisplaySettings::getSupportedAudioPorts, this);
-			Register<Params, JString>(_T("getSupportedAudioModes"), &DisplaySettings::getSupportedAudioModes, this);
-			Register<Params, JString>(_T("getZoomSetting"), &DisplaySettings::getZoomSetting, this);
-			Register<Params, JString>(_T("setZoomSetting"), &DisplaySettings::setZoomSetting, this);
-			Register<Params, JString>(_T("getCurrentResolution"), &DisplaySettings::getCurrentResolution, this);
-			Register<Params, JString>(_T("setCurrentResolution"), &DisplaySettings::setCurrentResolution, this);
-			Register<Params, JString>(_T("getSoundMode"), &DisplaySettings::getSoundMode, this);
-			Register<Params, JString>(_T("setSoundMode"), &DisplaySettings::setSoundMode, this);
-			Register<Params, JString>(_T("readEDID"), &DisplaySettings::readEDID, this);
-			Register<Params, JString>(_T("readHostEDID"), &DisplaySettings::readHostEDID, this);
-			Register<Params, JString>(_T("getActiveInput"), &DisplaySettings::getActiveInput, this);
-		}
-		DisplaySettings::~DisplaySettings()
-		{
-            printf("DisplaySettings DTOR!\n");
-			Unregister(_T("getConnectedVideoDisplays"));
-			Unregister(_T("getConnectedAudioPorts"));
-			Unregister(_T("getSupportedResolutions"));
-			Unregister(_T("getSupportedTvResolutions"));
-			Unregister(_T("getSupportedAudioPorts"));
-			Unregister(_T("getSupportedAudioModes"));
-			Unregister(_T("getZoomSetting"));
-			Unregister(_T("setZoomSetting"));
-			Unregister(_T("getCurrentResolution"));
-			Unregister(_T("setCurrentResolution"));
-			Unregister(_T("getSoundMode"));
-			Unregister(_T("setSoundMode"));
-			Unregister(_T("readEDID"));
-			Unregister(_T("readHostEDID"));
-			Unregister(_T("getActiveInput"));
-		}
-		const string DisplaySettings::Initialize(PluginHost::IShell* /* service */)
-		{
+        DisplaySettings* DisplaySettings::_instance = nullptr;
+
+        DisplaySettings::DisplaySettings()
+            : PluginHost::JSONRPC()
+        {
+            logger = fopen("/opt/logs/ds.log", "w");
+            printf("DisplaySettings logger=%p\n", logger);
+            fflush(stdout);
+            
+            MYLOG("DisplaySettings CTOR!\n");
+            DisplaySettings::_instance = this;
+            Register<Params, JStringArray >(_T("getConnectedVideoDisplays"), &DisplaySettings::getConnectedVideoDisplays, this);
+            Register<Params, JString>(_T("getConnectedAudioPorts"), &DisplaySettings::getConnectedAudioPorts, this);
+            Register<Params, JString>(_T("getSupportedResolutions"), &DisplaySettings::getSupportedResolutions, this);
+            Register<Params, JString>(_T("getSupportedTvResolutions"), &DisplaySettings::getSupportedTvResolutions, this);
+            Register<Params, JString>(_T("getSupportedAudioPorts"), &DisplaySettings::getSupportedAudioPorts, this);
+            Register<Params, JString>(_T("getSupportedAudioModes"), &DisplaySettings::getSupportedAudioModes, this);
+            Register<Params, JString>(_T("getZoomSetting"), &DisplaySettings::getZoomSetting, this);
+            Register<Params, JString>(_T("setZoomSetting"), &DisplaySettings::setZoomSetting, this);
+            Register<Params, JString>(_T("getCurrentResolution"), &DisplaySettings::getCurrentResolution, this);
+            Register<Params, JString>(_T("setCurrentResolution"), &DisplaySettings::setCurrentResolution, this);
+            Register<Params, JString>(_T("getSoundMode"), &DisplaySettings::getSoundMode, this);
+            Register<Params, JString>(_T("setSoundMode"), &DisplaySettings::setSoundMode, this);
+            Register<Params, JString>(_T("readEDID"), &DisplaySettings::readEDID, this);
+            Register<Params, JString>(_T("readHostEDID"), &DisplaySettings::readHostEDID, this);
+            Register<Params, JString>(_T("getActiveInput"), &DisplaySettings::getActiveInput, this);
+        }
+        DisplaySettings::~DisplaySettings()
+        {
+            MYLOG("DisplaySettings DTOR!\n");
+            DisplaySettings::_instance = nullptr;
+            Unregister(_T("getConnectedVideoDisplays"));
+            Unregister(_T("getConnectedAudioPorts"));
+            Unregister(_T("getSupportedResolutions"));
+            Unregister(_T("getSupportedTvResolutions"));
+            Unregister(_T("getSupportedAudioPorts"));
+            Unregister(_T("getSupportedAudioModes"));
+            Unregister(_T("getZoomSetting"));
+            Unregister(_T("setZoomSetting"));
+            Unregister(_T("getCurrentResolution"));
+            Unregister(_T("setCurrentResolution"));
+            Unregister(_T("getSoundMode"));
+            Unregister(_T("setSoundMode"));
+            Unregister(_T("readEDID"));
+            Unregister(_T("readHostEDID"));
+            Unregister(_T("getActiveInput"));
+        }
+        const string DisplaySettings::Initialize(PluginHost::IShell* /* service */)
+        {
 #ifdef DS_FOUND
             InitializeIARM();
-#endif		
-			// On success return empty, to indicate there is no error text.
-			return (string());
-		}
-		void DisplaySettings::Deinitialize(PluginHost::IShell* /* service */)
-		{
+#endif        
+            // On success return empty, to indicate there is no error text.
+            return (string());
+        }
+        void DisplaySettings::Deinitialize(PluginHost::IShell* /* service */)
+        {
 #ifdef DS_FOUND
             DeinitializeIARM();
-#endif		
-		}
-		string DisplaySettings::Information() const
-		{
-			// No additional info to report.
-			return (string());
-		}
+#endif        
+        }
+        string DisplaySettings::Information() const
+        {
+            // No additional info to report.
+            return (string());
+        }
 #ifdef DS_FOUND
         void DisplaySettings::InitializeIARM()
         {
-            IARM_Bus_Init("Display_Settings");
-            IARM_Bus_Connect();
-            IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_RX_SENSE, DisplResolutionHandler);
-            IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_ZOOM_SETTINGS,DisplResolutionHandler);
+            MYLOG("%s\n", __PRETTY_FUNCTION__);        
+            IARM_Result_t res;
+            IARM_CHECK( IARM_Bus_Init("Display_Settings") );
+            IARM_CHECK( IARM_Bus_Connect() );
+            IARM_CHECK( IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_RX_SENSE, DisplResolutionHandler) );
+            IARM_CHECK( IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_ZOOM_SETTINGS, DisplResolutionHandler) );
             //TODO/FIXME localinput.cpp has PreChange guared with #if !defined(DISABLE_PRE_RES_CHANGE_EVENTS)
             //Can we set it all the time from inside here and let localinput put guards around listening for our event?
-            IARM_Bus_RegisterCall(IARM_BUS_COMMON_API_ResolutionPreChange, ResolutionPreChange);
-            IARM_Bus_RegisterCall(IARM_BUS_COMMON_API_ResolutionPostChange, ResolutionPostChange);            
+            IARM_CHECK( IARM_Bus_RegisterCall(IARM_BUS_COMMON_API_ResolutionPreChange, ResolutionPreChange) );
+            IARM_CHECK( IARM_Bus_RegisterCall(IARM_BUS_COMMON_API_ResolutionPostChange, ResolutionPostChange) );
             //TODO/FIXME IS THE FOLLOWING REQUIRED -- localinput.cpp was doing it
             try
             {
                 device::Manager::Initialize();
+                MYLOG("device::Manager::Initialize success\n");
             }
             catch(...)
             {
+                MYLOG("device::Manager::Initialize fail\n");            
             }
         }
         //TODO/FIXME - we need to install crash handler to ensure DeinitializeIARM gets called
         void DisplaySettings::DeinitializeIARM()
         {
-            IARM_Bus_UnRegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_RX_SENSE);
-            IARM_Bus_UnRegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_ZOOM_SETTINGS);
-            IARM_Bus_Disconnect();
-            IARM_Bus_Term();
+            MYLOG("%s\n", __PRETTY_FUNCTION__);        
+            IARM_Result_t res;        
+            IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_RX_SENSE) );
+            IARM_CHECK( IARM_Bus_UnRegisterEventHandler(IARM_BUS_DSMGR_NAME, IARM_BUS_DSMGR_EVENT_ZOOM_SETTINGS) );
+            IARM_CHECK( IARM_Bus_Disconnect() );
+            IARM_CHECK( IARM_Bus_Term() );
             //TODO/FIXME IS THE FOLLOWING REQUIRED -- localinput.cpp was doing it
             try
             {
                 device::Manager::DeInitialize();
+                MYLOG("device::Manager::DeInitialize success\n");
             }
             catch(...)
             {
+                MYLOG("device::Manager::DeInitialize fail\n");            
             }
             
         }
-		static IARM_Result_t DisplaySettings::ResolutionPreChange(void *arg)
-		{
-    		return IARM_RESULT_SUCCESS;
-		}
-		static IARM_Result_t DisplaySettings::ResolutionPostChange(void *arg)
-		{
+        IARM_Result_t DisplaySettings::ResolutionPreChange(void *arg)
+        {
+            MYLOG("%s\n", __PRETTY_FUNCTION__);
+            return IARM_RESULT_SUCCESS;
+        }
+        IARM_Result_t DisplaySettings::ResolutionPostChange(void *arg)
+        {
+            MYLOG("%s\n", __PRETTY_FUNCTION__);        
             int dw = 1280;
             int dh = 720;
             IARM_Bus_CommonAPI_ResChange_Param_t *eventData = (IARM_Bus_CommonAPI_ResChange_Param_t *)arg;
             dw = eventData->width ;
             dh = eventData->height ;
-		
-    		return IARM_RESULT_SUCCESS;
+            if(DisplaySettings::_instance)
+                DisplaySettings::_instance->resolutionChanged(dw,dh);        
+            return IARM_RESULT_SUCCESS;
         }
-        static void DisplResolutionHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len)
+        void DisplaySettings::DisplResolutionHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len)
         {
+               MYLOG("%s\n", __PRETTY_FUNCTION__);
             //TODO/FIXME Receiver has this whole think guarded by #ifndef HEADLESS_GW
             if (strcmp(owner,IARM_BUS_DSMGR_NAME) == 0)
             {
@@ -130,7 +186,8 @@ namespace WPEFramework {
                             IARM_Bus_DSMgr_EventData_t *eventData = (IARM_Bus_DSMgr_EventData_t *)data;
                             dw = eventData->data.resn.width ;
                             dh = eventData->data.resn.height ;
-                            //postResolutionChange(dw,dh);
+                            if(DisplaySettings::_instance)
+                                DisplaySettings::_instance->resolutionChanged(dw,dh);
                         }
                         break;
                     case IARM_BUS_DSMGR_EVENT_ZOOM_SETTINGS:
@@ -138,12 +195,12 @@ namespace WPEFramework {
                             IARM_Bus_DSMgr_EventData_t *eventData = (IARM_Bus_DSMgr_EventData_t *)data;
                             if(eventData->data.dfc.zoomsettings == dsVIDEO_ZOOM_NONE)
                             {
-                                printf("%s: dsVIDEO_ZOOM_NONE Settings  ",__FUNCTION__);
+                                MYLOG("%s: dsVIDEO_ZOOM_NONE Settings\n",__FUNCTION__);
                                 //ServiceManagerNotifier::getInstance()->notifySetZoom(1);
                             }
                             else if(eventData->data.dfc.zoomsettings == dsVIDEO_ZOOM_FULL)
                             {
-                                printf("%s: dsVIDEO_ZOOM_FULL Settinmgs ",__FUNCTION__);
+                                MYLOG("%s: dsVIDEO_ZOOM_FULL Settinmgs\n",__FUNCTION__);
                                 //ServiceManagerNotifier::getInstance()->notifySetZoom(0);
                             }
                         }
@@ -154,12 +211,12 @@ namespace WPEFramework {
                             IARM_Bus_DSMgr_EventData_t *eventData = (IARM_Bus_DSMgr_EventData_t *)data;
                             if(eventData->data.hdmi_rxsense.status == dsDISPLAY_RXSENSE_ON)
                             {
-                                printf("%s: Got dsDISPLAY_RXSENSE_ON -> notifyactiveInputChanged(true)!!!!  ",__FUNCTION__);
+                                MYLOG("%s: Got dsDISPLAY_RXSENSE_ON -> notifyactiveInputChanged(true)\n",__FUNCTION__);
                                 //ServiceManagerNotifier::getInstance()->notifyactiveInputChanged(true); 
                             }
                             else if(eventData->data.hdmi_rxsense.status == dsDISPLAY_RXSENSE_OFF)
                             {
-                                printf("%s: Got dsDISPLAY_RXSENSE_OFF -> notifyactiveInputChanged(false)!!!! ",__FUNCTION__);
+                                MYLOG("%s: Got dsDISPLAY_RXSENSE_OFF -> notifyactiveInputChanged(false)\n",__FUNCTION__);
                                 //ServiceManagerNotifier::getInstance()->notifyactiveInputChanged(false);
                             }
                         }
@@ -171,6 +228,6 @@ namespace WPEFramework {
         }
 #endif
         
-	} // namespace Plugin
-	
+    } // namespace Plugin
+    
 } // namespace WPEFramework
--- DisplaySettings.h
+++ DisplaySettings.h
@@ -11,18 +11,18 @@ namespace WPEFramework {
 
     namespace Plugin {
 
-		// This is a server for a JSONRPC communication channel. 
-		// For a plugin to be capable to handle JSONRPC, inherit from PluginHost::JSONRPC.
-		// By inheriting from this class, the plugin realizes the interface PluginHost::IDispatcher.
-		// This realization of this interface implements, by default, the following methods on this plugin
-		// - exists
-		// - register
-		// - unregister
-		// Any other methood to be handled by this plugin  can be added can be added by using the
-		// templated methods Register on the PluginHost::JSONRPC class.
-		// As the registration/unregistration of notifications is realized by the class PluginHost::JSONRPC,
-		// this class exposes a public method called, Notify(), using this methods, all subscribed clients
-		// will receive a JSONRPC message as a notification, in case this method is called.
+        // This is a server for a JSONRPC communication channel. 
+        // For a plugin to be capable to handle JSONRPC, inherit from PluginHost::JSONRPC.
+        // By inheriting from this class, the plugin realizes the interface PluginHost::IDispatcher.
+        // This realization of this interface implements, by default, the following methods on this plugin
+        // - exists
+        // - register
+        // - unregister
+        // Any other methood to be handled by this plugin  can be added can be added by using the
+        // templated methods Register on the PluginHost::JSONRPC class.
+        // As the registration/unregistration of notifications is realized by the class PluginHost::JSONRPC,
+        // this class exposes a public method called, Notify(), using this methods, all subscribed clients
+        // will receive a JSONRPC message as a notification, in case this method is called.
         class DisplaySettings : public PluginHost::IPlugin, public PluginHost::JSONRPC {
         private:
             // We do not allow this plugin to be copied !!
@@ -38,6 +38,8 @@ namespace WPEFramework {
                     Add("zoomLevel", &zoomLevel);
                     Add("soundMode", &soundMode);
                     Add("audioPort", &audioPort);
+                    Add("width", &width);
+                    Add("height", &height);
                 }
                 Params(const Params& copy)
                 {
@@ -46,11 +48,15 @@ namespace WPEFramework {
                     Add("zoomLevel", &zoomLevel);
                     Add("soundMode", &soundMode);
                     Add("audioPort", &audioPort);
+                    Add("width", &width);
+                    Add("height", &height);
                     videoDisplay = copy.videoDisplay;
                     resolution = copy.resolution;
                     zoomLevel = copy.zoomLevel;
                     soundMode = copy.soundMode;
                     audioPort = copy.audioPort;
+                    width = copy.width;
+                    height = copy.height;
                 }
 
                 Params& operator=(const Params& copy)
@@ -60,6 +66,8 @@ namespace WPEFramework {
                     zoomLevel = copy.zoomLevel;
                     soundMode = copy.soundMode;
                     audioPort = copy.audioPort;
+                    width = copy.width;
+                    height = copy.height;
                     return *this;
                 }
             public:
@@ -68,6 +76,8 @@ namespace WPEFramework {
                 Core::JSON::String zoomLevel;
                 Core::JSON::String soundMode;
                 Core::JSON::String audioPort;
+                Core::JSON::DecSInt32 width;
+                Core::JSON::DecSInt32 height;
             };
 
             typedef Core::JSON::String JString;
@@ -157,12 +167,18 @@ namespace WPEFramework {
                 return (Core::ERROR_NONE);
             }
             //End methods
+        public:
             //Begin events
             void connectedVideoDisplaysUpdated() {
                 Notify(_T("connectedVideoDisplaysUpdated"), Core::JSON::String("\"NOTIMPL\""));
             }
-            void resolutionChanged() {
-                Notify(_T("resolutionChanged"), Core::JSON::String("\"NOTIMPL\""));
+            void resolutionChanged(int width, int height) {
+                Params params;
+                params.width = width;
+                params.height = height;
+                string result;
+                params.ToString(result);
+                Notify(_T("resolutionChanged"), result);
             }
             void zoomSettingUpdated() {
                 Notify(_T("zoomSettingUpdated"), Core::JSON::String("\"NOTIMPL\""));
@@ -171,7 +187,6 @@ namespace WPEFramework {
                 Notify(_T("activeInputChanged"), Core::JSON::String("\"NOTIMPL\""));
             }
             //End events
-        public:
             DisplaySettings();
             virtual ~DisplaySettings();
             //Build QueryInterface implementation, specifying all possible interfaces to be returned.
@@ -187,10 +202,12 @@ namespace WPEFramework {
 #ifdef DS_FOUND
             void InitializeIARM();
             void DeinitializeIARM();
-		    static IARM_Result_t ResolutionPreChange(void *arg);
-		    static IARM_Result_t ResolutionPostChange(void *arg);
+            static IARM_Result_t ResolutionPreChange(void *arg);
+            static IARM_Result_t ResolutionPostChange(void *arg);
             static void DisplResolutionHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
 #endif
+        public:
+            static DisplaySettings* _instance;
         };
-	} // namespace Plugin
+    } // namespace Plugin
 } // namespace WPEFramework
